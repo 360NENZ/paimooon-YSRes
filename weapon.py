f98bd3ee77ef36fe5dd4e8a79796ef1df6669842
ion <ion@example.com>
2023-01-21 05:39:24 +0000

    Weapon excel output support (thx 浩君)

--- weapon.py
+++ weapon.py
@@ -2,6 +2,7 @@ import json
 import os
 from pathlib import Path
 import re
+import xlsxwriter
 
 def weaponExtraction(textmap, weaponID):
     files = {"WeaponExcelConfigData": {},  # Main stats
@@ -13,7 +14,7 @@ def weaponExtraction(textmap, weaponID):
             }
 
     for file in files:
-        with open(os.path.join(os.path.dirname(__file__), f'./json/{file}.json')) as json_file:
+        with open(os.path.join(os.path.dirname(__file__), f'./json/{file}.json'), encoding='utf-8') as json_file:
             files[file] = json.load(json_file)
 
     weapon(textmap, weaponID, files)
@@ -83,79 +84,124 @@ def weapon(textmap, weaponID, files):
     }
     
     os.makedirs(os.path.join(os.path.dirname(__file__), f'res/'), exist_ok=True)
-    with open(os.path.join(os.path.dirname(__file__), f'res/{weaponID}.json'), 'w') as output_file:
+    with open(os.path.join(os.path.dirname(__file__), f'res/{weaponID}.json'), 'w', encoding='utf-8') as output_file:
         json.dump(json_dict, output_file, indent=4, ensure_ascii=False)
 
 def ConvertText(desc):
     return "".join(re.split("<color.*?>(.+?)</color>", desc.replace("\\n", "\n")))
 
 def GenerateRes(parseWeaponID, textMapLanguage):
-    with open(os.path.join(os.path.dirname(__file__), f'json/TextMap_{textMapLanguage}.json')) as textmap_json:
+    with open(os.path.join(os.path.dirname(__file__), f'json/TextMap_{textMapLanguage}.json'), encoding='utf-8') as textmap_json:
         textmap = json.load(textmap_json)
 
     weaponExtraction(textmap, parseWeaponID)
 
-    with open(os.path.join(os.path.dirname(__file__), f'res/{parseWeaponID}.json')) as dump:
+    with open(os.path.join(os.path.dirname(__file__), f'res/{parseWeaponID}.json'), encoding='utf-8') as dump:
         res = json.load(dump)
-    
-    weapon_dic = {
-        "WEAPON_BOW": "Bow",
-        "WEAPON_CATALYST": "Catalyst",
-        "WEAPON_CLAYMORE": "Claymore",
-        "WEAPON_POLE": "Polearm",
-        "WEAPON_SWORD_ONE_HAND": "Sword"
-    }
-
-    asc_dic = {
-        "FIGHT_PROP_HP_PERCENT": "HP%",
-        "FIGHT_PROP_ATTACK_PERCENT": "ATK%",
-        "FIGHT_PROP_DEFENSE_PERCENT": "DEF%",
-        "FIGHT_PROP_ELEMENT_MASTERY": "Elemental Mastery",
-        "FIGHT_PROP_CHARGE_EFFICIENCY": "Energy Recharge",
-        "FIGHT_PROP_HEAL_ADD": "Heal Bonus",
-        "FIGHT_PROP_CRITICAL_HURT": "Crit Damage",
-        "FIGHT_PROP_CRITICAL": "Crit Rate",
-        "FIGHT_PROP_PHYSICAL_ADD_HURT": "Phys Bonus",
-        "FIGHT_PROP_GRASS_ADD_HURT": "Dendro Bonus",
-        "FIGHT_PROP_ROCK_ADD_HURT": "Geo Bonus",
-        "FIGHT_PROP_WIND_ADD_HURT": "Anemo Bonus",
-        "FIGHT_PROP_WATER_ADD_HURT": "Hydro Bonus",
-        "FIGHT_PROP_ICE_ADD_HURT": "Cryo Bonus",
-        "FIGHT_PROP_ELEC_ADD_HURT": "Electro Bonus",
-        "FIGHT_PROP_FIRE_ADD_HURT": "Pyro Bonus"
-    }
-
-    print("\n==========")
-    print(res["Name"])
-    print(res["Desc"])
-    print(f'Rarity: {res["Rarity"]}')
-    print(f'Type: {weapon_dic[res["WeaponType"]]}')
-
-    for stat in res["StatsModifier"].keys():
-        statCalc = res["StatsModifier"][stat]["Base"] * res["StatsModifier"][stat]["Levels"]["90"]
         
-        if stat == "ATK":
-            print(f'Base ATK: {int(statCalc + res["Ascension"]["6"]["FIGHT_PROP_BASE_ATTACK"])}')
-        elif stat == "FIGHT_PROP_ELEMENT_MASTERY":
-            print(f"{asc_dic[stat]}: {'{:0.0f}'.format(statCalc)}")
-        else:
-            print(f"{asc_dic[stat]}: {'{:.1%}'.format(statCalc)}")
-    
-    print()
+        wb = xlsxwriter.Workbook(f'./res/{parseWeaponID}.xlsx')
+        ws = wb.add_worksheet()
 
-    for i in "123456":
-        items = []
-        for j in res["Materials"][i]["Mats"]:
-            items.append(j["Name"] + " x" + str(j["Count"]))
+        name_format = wb.add_format({
+            'bold': True,
+            'align': 'center',
+            'valign': 'vjustify',
+            'border': 1
+        })
 
-        print(", ".join(items))
+        desc_format = wb.add_format({
+            'align': 'center',
+            'valign': 'vjustify',
+            'text_wrap': True,
+            'border': 1
+        })
 
-    print()
+        ws.merge_range('A1:H1', res["Name"], name_format)
+        
+        weapon_dic = {
+            "WEAPON_BOW": "Bow",
+            "WEAPON_CATALYST": "Catalyst",
+            "WEAPON_CLAYMORE": "Claymore",
+            "WEAPON_POLE": "Polearm",
+            "WEAPON_SWORD_ONE_HAND": "Sword"
+        }
 
-    print(res["Refinement"]["1"]["Name"])
-    for i in "12345":
-        items = []
-        print("\nRefinement Level", i)
-        print(ConvertText(res["Refinement"][i]["Desc"]))
+        asc_dic = {
+            "FIGHT_PROP_HP_PERCENT": "HP%",
+            "FIGHT_PROP_ATTACK_PERCENT": "ATK%",
+            "FIGHT_PROP_DEFENSE_PERCENT": "DEF%",
+            "FIGHT_PROP_ELEMENT_MASTERY": "Elemental Mastery",
+            "FIGHT_PROP_CHARGE_EFFICIENCY": "Energy Recharge",
+            "FIGHT_PROP_HEAL_ADD": "Heal Bonus",
+            "FIGHT_PROP_CRITICAL_HURT": "Crit Damage",
+            "FIGHT_PROP_CRITICAL": "Crit Rate",
+            "FIGHT_PROP_PHYSICAL_ADD_HURT": "Phys Bonus",
+            "FIGHT_PROP_GRASS_ADD_HURT": "Dendro Bonus",
+            "FIGHT_PROP_ROCK_ADD_HURT": "Geo Bonus",
+            "FIGHT_PROP_WIND_ADD_HURT": "Anemo Bonus",
+            "FIGHT_PROP_WATER_ADD_HURT": "Hydro Bonus",
+            "FIGHT_PROP_ICE_ADD_HURT": "Cryo Bonus",
+            "FIGHT_PROP_ELEC_ADD_HURT": "Electro Bonus",
+            "FIGHT_PROP_FIRE_ADD_HURT": "Pyro Bonus"
+        }
+        
+        ws.merge_range('A2:B2', "Rarity", name_format)
+        ws.merge_range('C2:D2', res["Rarity"], desc_format)
 
-    print("==========")
+        ws.merge_range('A3:B3', "Weapon", name_format)
+        ws.merge_range('C3:D3', weapon_dic[res["WeaponType"]], desc_format)
+        
+        ws.merge_range('E2:F2', "Base ATK", name_format)
+
+        print("\n==========")
+        print(res["Name"])
+        print(res["Desc"])
+        print(f'Rarity: {res["Rarity"]}')
+        print(f'Type: {weapon_dic[res["WeaponType"]]}')
+
+        for stat in res["StatsModifier"].keys():
+            statCalc = res["StatsModifier"][stat]["Base"] * res["StatsModifier"][stat]["Levels"]["90"]
+            
+            if stat == "ATK":
+                print(f'Base ATK: {int(statCalc + res["Ascension"]["6"]["FIGHT_PROP_BASE_ATTACK"])}')
+                ws.merge_range('G2:H2', res["Rarity"], desc_format)
+            elif stat == "FIGHT_PROP_ELEMENT_MASTERY":
+                print(f"{asc_dic[stat]}: {'{:0.0f}'.format(statCalc)}")
+                ws.merge_range('E3:F3', asc_dic[stat], name_format)
+                ws.merge_range('G3:H3', '{:0.0f}'.format(statCalc), desc_format )
+            else:
+                print(f"{asc_dic[stat]}: {'{:.1%}'.format(statCalc)}")
+                ws.merge_range('E3:F3', asc_dic[stat], name_format)
+                ws.merge_range('G3:H3', '{:.1%}'.format(statCalc), desc_format )
+        
+        print()
+
+        ws.merge_range('A6:J6', "Promote Costs", name_format)
+
+        for i in range(6):
+            items = []
+            ws.write(i+6, 0, i+1, name_format)
+            for j in res["Materials"][str(i+1)]["Mats"]:
+                #print(i)
+                items.append(j["Name"] + " x" + str(j["Count"]))
+            
+            print(", ".join(items))
+            ws.merge_range(i+6, 1, i+6, 9, ", ".join(items), desc_format)
+        print()
+
+
+        ws.merge_range('A14:L14', res["Refinement"]["1"]["Name"], name_format)
+
+        print(res["Refinement"]["1"]["Name"])
+        for i in range(5):
+            items = []
+            print("\nRefinement Level", i)
+            print(ConvertText(res["Refinement"][str(i+1)]["Desc"]))
+            
+            ws.merge_range(i+14, 0, i+14, 1, "Refinement Level " + str( i + 1), name_format)
+            ws.merge_range(i+14, 2, i+14, 11, ConvertText(res["Refinement"][str(i+1)]["Desc"]), desc_format)
+            ws.set_row(i+14, 100)
+
+        print("==========")
+        
+        wb.close()
